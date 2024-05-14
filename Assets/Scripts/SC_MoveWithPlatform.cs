39f463b3cf6a4ef8c8f08f88fe0e3f8b3883bf45
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2024-05-14 11:13:22 +0300

    Added goomba
    walks left and right and damages player on contact

--- Assets/Scripts/SC_MoveWithPlatform.cs
+++ Assets/Scripts/SC_MoveWithPlatform.cs
@@ -6,12 +6,12 @@ public class SC_MoveWithPlatform : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Resource"))
+        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Resource") || collision.gameObject.CompareTag("Enemy"))
             collision.transform.SetParent(transform);
     }
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
             collision.transform.SetParent(null);
     }
 }
---
+++ Assets/Scripts/SC_SimpleLeftRightWalk.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_SimpleLeftRightWalk : MonoBehaviour
+{
+    public float speed = 3f;
+    public float changeDirectionTime = 2f;
+    public float startDirection = 1f;
+    private float direction;
+    private Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        direction = startDirection;
+        StartCoroutine(WaitThenChangeDirection());
+    }
+    
+    void FixedUpdate()
+    {
+        if (direction != 0 && rigid != null)
+        {
+            rigid.velocity = new Vector2(direction * speed, rigid.velocity.y);
+
+            if (direction > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    IEnumerator WaitThenChangeDirection()
+    {
+        yield return new WaitForSeconds(changeDirectionTime);
+        if (gameObject.activeSelf)
+        {
+            direction *= -1;
+            StartCoroutine(WaitThenChangeDirection());
+        }
+    }
+}
